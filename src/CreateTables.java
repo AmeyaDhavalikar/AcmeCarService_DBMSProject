39b1eb0c8e3d70fbd9687bd1ba65b5013cf18de5
Ameya <adhaval@ncsu.edu>
2018-10-27 17:36:10 -0400

    Skeleton code to create tables

---
+++ src/CreateTables.java
@@ -0,0 +1,61 @@
+import java.sql.*;
+
+public class CreateTables {
+
+	static final String jdbcURL = "jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01";
+
+    public static void main(String[] args) {
+        try {
+
+       // Load the driver. This creates an instance of the driver
+	   // and calls the registerDriver method to make Oracle Thin
+	   // driver available to clients.
+
+       Class.forName("oracle.jdbc.driver.OracleDriver");
+            
+       String user = "adhaval";	// For example, "jsmith"
+       String passwd = "200263183";	// Your 9 digit student ID number or password
+
+       Connection conn = null;
+       Statement stmt = null;
+       ResultSet rs = null;
+       try {
+                // Get a connection from the first driver in the
+            	// DriverManager list that recognizes the URL jdbcURL
+
+            	conn = DriverManager.getConnection(jdbcURL, user, passwd);
+
+            	// Create a statement object that will be sending your
+            	// SQL statements to the DBMS
+
+            	stmt = conn.createStatement();
+                
+        } finally {
+        close(rs);
+        close(stmt);
+        close(conn);
+        } } catch(Throwable oops) {
+        oops.printStackTrace();
+    }
+        
+}
+
+    static void close(Connection conn) {
+    	if(conn != null) {
+    		try { conn.close(); } catch(Throwable whatever) {}
+    	}
+    }//close connection
+
+	static void close(Statement st) {
+		if(st != null) {
+			try { st.close(); } catch(Throwable whatever) {}
+		}
+	}//close statement
+
+	static void close(ResultSet rs) {
+		if(rs != null) {
+			try { rs.close(); } catch(Throwable whatever) {}
+		}
+	}//close resultset
+	
+}
